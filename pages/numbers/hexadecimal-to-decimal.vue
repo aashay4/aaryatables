d0b56ad72f1d5c4596ed0c131f3aa4fedbc94312
Aashay <aashay.trivedi1@gmail.com>
2022-02-08 12:11:13 +0530

    first

--- pages/numbers/hexadecimal-to-decimal.vue
+++ pages/numbers/hexadecimal-to-decimal.vue
@@ -13,6 +13,14 @@
     <div class="w3-container w3-padding">
           <a href="/">Home</a> &rsaquo; <a href="/numbers">Numbers</a> &rsaquo; Hexadecimal to Decimal<hr>
       <h1 class="w3-text-teal"><b>Hexadecimal to Decimal</b></h1>
+      <Adsense
+      class="adsbygoogle infeed"
+      style="display:block"
+      data-ad-client="ca-pub-6829148792481216"
+      data-ad-slot="4199493091"
+      data-ad-format="auto"
+      data-full-width-responsive="true">
+    </Adsense>
       <p>Hexadecimal to decimal conversion tool.</p>
       <form id="calcform1" name="calcform1" autocomplete="off" class="rounded">
 <div class="form-row">
@@ -151,6 +159,24 @@ export default {
 </script>
 
 <style lang="css">
+@keyframes cssAnimation {
+  to   { visibility: visible; }
+}
+@media (min-width: 350px) {
+    .infeed {
+      height: 290px;
+    }
+  }
+ @media (min-width: 500px) {
+    .infeed {
+      height: 290px;
+    }
+  }
+ @media (min-width: 800px) {
+    .infeed {
+      height: 290px;
+    }
+  }
 h1 { font-size:1.6rem; }
 h2 { font-size:1.4rem; }
 h3 { font-size:1.4rem; }
