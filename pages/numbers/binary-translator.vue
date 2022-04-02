4973b7526b50cd7f915c97b84b101d384e68c760
Aashay <aashay.trivedi1@gmail.com>
2022-04-02 17:46:15 +0530

    first

--- pages/numbers/binary-translator.vue
+++ pages/numbers/binary-translator.vue
@@ -12,7 +12,7 @@
 
     <div class="w3-container w3-padding">
           <a href="/">Home</a> &rsaquo; <a href="/numbers">Numbers</a> &rsaquo; Binary Translator<hr>
-      <h1 class="w3-text-teal"><b>Binary Translator - Binary to Text(English)</b></h1>
+      <h1 class="w3-text-teal"><b>Binary Translator - Binary to English</b></h1>
       <Adsense
       class="adsbygoogle infeed"
       style="display:block"
@@ -21,7 +21,7 @@
       data-ad-format="auto"
       data-full-width-responsive="true">
     </Adsense>
-      <p>Use this binary translator to convert binary to text and other numbering systems. Enter a binary number and click on the answer button for binary translation. For example, enter a binary number <b>01000001</b> in the box and click on the answer button. You will get an A as the binary to text conversion.</p>
+      <p>Use this binary code translator to convert binary to ASCII and other numbering systems. Enter a binary number and click on the answer button for binary translation. For example, enter a binary number <b>01000001</b> in the box and click on the answer button. You will get an A as the binary to word conversion.</p>
       <form id="theraldesign" name="theraldesign" class="rounded">
 <div class="form-row">
 <div class="col">
@@ -81,12 +81,12 @@
 </form>
 </div><br>
 <div class="w3-padding w3-raw w3-align">
-<h2 class="w3-center"><b>Binary Translator</b></h2>
-<p><b>Binary translator</b> is a tool that helps users to convert binary to all other major numbering systems. Along with all other numbering systems, users can convert binary to English (text) as well.</p>
+<h2 class="w3-center"><b>Translate binary code</b></h2>
+<p><b>Binary code translator</b> is a tool that helps users to convert binary to all other major numbering systems. Along with all other numbering systems, users can convert binary to English (text) as well.</p>
 <h2>About Binary Numbering System</h2>
 <p>Binary numbering system contains just zeros and ones. Many electronics systems use binary in the processors to perform computer organization operations. For that reason, it is a base-2 number. It has become an integral part of modern electronics. Earlier, many developers used to complain about the size of a binary number. To store data in just zeros and ones was difficult. Binary occupies more space on a machine. But today, with the help of supercomputers and decent storage facilities, it has become easy to store binary.</p>
 <p>Earlier, many developers used to complain about the size of a binary number. To store data in just zeros and ones was difficult and it used to occupy more space on a machine. But today with the help of supercomputers and decent storage facilities, it has become very easy to store binary.</p>
-<h1><b>Binary to Text</b></h1>
+<h1><b>Convert Binary to English</b></h1>
 <p>The binary to text conversion is one of the most common conversions for computer programmers. It is necessary for the machines that take input from the humans and instruct the machine's processor in the language it understands - Binary. To develop such apps, developers often need to use a translator to convert binary to text. With the help of a binary converter, a developer converts the user input to binary then the server sends binary to the microprocessor. The microprocessor processes the binary and converts response binary to text, sends it back to the users.</p>
 <p>It is the most basic working of a binary translator. Many NoSQL databases implement the concepts of binary numbers to store images and then process and representing data faster.</p>
   </div>
@@ -125,9 +125,6 @@
               <span class="w3-large"><a class="removelink" href="/numbers/hexadecimal-to-decimal"> Hex to Decimal</a></span><br>
             </li>
             <li class="w3-padding-16">
-              <span class="w3-large"><a class="removelink" href="/numbers/binary-to-text"> Binary to Text</a></span><br>
-            </li>
-            <li class="w3-padding-16">
               <span class="w3-large"><a class="removelink" href="/numbers/text-to-binary"> Text to Binary</a></span><br>
             </li>
           </ul>
@@ -159,7 +156,7 @@ export default {
   head() {
     // sets document title
     return {
-      title: 'Binary Translator - Convert Binary to text(English)',
+      title: 'Binary translator to text (English)',
   // optional; sets final title as "Index Page - My Website", useful for multiple level meta
   // meta tags
   meta: [
